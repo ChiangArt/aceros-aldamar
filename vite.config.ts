7a55af6143214e4f38725b153229850c530a81d8
Bryan Lee Chiang Arteaga <bryan_94tj@hotmail.com>
2026-05-04 17:50:16 -0500

    automatization

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+    base: "/aceros-aldamar/",
   plugins: [tailwindcss(), reactRouter()],
   resolve: {
     tsconfigPaths: true,
