9e06e43f090de912365fda80c06dc1ff3b413809
Bryan Lee Chiang Arteaga <bryan_94tj@hotmail.com>
2026-05-13 14:50:05 -0500

    fixed: duplicate url

--- app/routes.ts
+++ app/routes.ts
@@ -3,7 +3,7 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 export default [
   index("routes/home.tsx"),
   route("productos", "routes/productos.tsx"),
-  route("productos/:categoria", "routes/productos.tsx"),
+  route("productos/:categoria", "routes/productos.tsx", { id: "productos-categoria" }),
   route("producto/:productId", "routes/producto.tsx"),
   route("nosotros", "routes/nosotros.tsx"),
   route("contacto", "routes/contacto.tsx"),
